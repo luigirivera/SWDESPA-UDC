7eb50e19ad3a28b8f7933450da6f0746da5d4e64
Luigi Rivera <luigiriveraiv@gmail.com>
2018-03-29 22:17:48 +0800

    Secretary View almost working GUI-wise

---
+++ SWDESPA-UDC/src/ultimatedesignchallenge/UDCSecretaryDriver.java
@@ -0,0 +1,9 @@
+package ultimatedesignchallenge;
+
+import ultimatedesignchallenge.view.SecretaryView;
+
+public class UDCSecretaryDriver {
+	public static void main(String[] args) {
+		new SecretaryView();
+	}
+}
