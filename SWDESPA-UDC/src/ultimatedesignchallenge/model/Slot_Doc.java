e9e7167c945ff085428ec3967d5eaa4afd1b5445
Luigi Rivera <luigiriveraiv@gmail.com>
2018-04-12 23:35:12 +0800

    Slot_Doc

---
+++ SWDESPA-UDC/src/ultimatedesignchallenge/model/Slot_Doc.java
@@ -0,0 +1,7 @@
+package ultimatedesignchallenge.model;
+
+public class Slot_Doc {
+	public static final String TABLE = "SLOT_DOC";
+	public static final String COL_SLOTID = "SLOTid";
+	public static final String COL_DOCTORID = "DOCTORid";
+}
