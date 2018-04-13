221213ea6318d4ef2281c62906e954b546fe9078
Luigi Rivera <luigiriveraiv@gmail.com>
2018-04-13 14:25:17 +0800

    slot doc fix

--- SWDESPA-UDC/src/ultimatedesignchallenge/model/Slot_Doc.java
+++ SWDESPA-UDC/src/ultimatedesignchallenge/model/Slot_Doc.java
@@ -1,8 +1,8 @@
 package ultimatedesignchallenge.model;
 
 public class Slot_Doc extends Slot{
+	
 	public static final String TABLE = "SLOT_DOC";
-	public static final String COL_SLOTID = "SLOTid";
 	public static final String COL_DOCTORID = "DOCTORid";
 	
 	private String DOCTORid;
