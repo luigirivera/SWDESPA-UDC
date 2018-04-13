5d063170bae8efd5e06b55e21035a28dd01c2e2c
Luis Lopez <luis_lopez@dlsu.edu.ph>
2018-04-13 21:52:40 +0800

    Removed password attribute from User

--- SWDESPA-UDC/src/ultimatedesignchallenge/model/User.java
+++ SWDESPA-UDC/src/ultimatedesignchallenge/model/User.java
@@ -1,12 +1,11 @@
 package ultimatedesignchallenge.model;
 
 public class User {
-	private int USERid;
+	private final int id;
 	private String username;
-	private String password;
 	private String firstname;
 	private String lastname;
-	
+
 	public static final String TABLE = "USER";
 	public static final String COL_USERID = "USERid";
 	public static final String COL_USERNAME = "username";
@@ -14,44 +13,36 @@ public class User {
 	public static final String COL_FIRSTNAME = "firstname";
 	public static final String COL_LASTNAME = "lastname";
 	
-	public int getUSERid() {
-		return USERid;
+	public User(int id) {
+		this.id = id;
 	}
-	
-	public void setUSERid(int uSERid) {
-		USERid = uSERid;
+
+	public int getId() {
+		return id;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+
 	public String getFirstname() {
 		return firstname;
 	}
-	
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
-	
+
 	public String getLastname() {
 		return lastname;
 	}
-	
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-	
+
 }
