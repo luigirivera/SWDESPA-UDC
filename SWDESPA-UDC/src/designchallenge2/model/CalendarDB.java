58b466bea2332bdf8b55e22c6435c5f92562195c
tadhg <tadhgmccarthy03@gmail.com>
2018-04-13 11:40:43 +0800

    singleton attempt #1

--- SWDESPA-UDC/src/designchallenge2/model/CalendarDB.java
+++ SWDESPA-UDC/src/designchallenge2/model/CalendarDB.java
@@ -1,6 +1,5 @@
 package designchallenge2.model;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -11,25 +10,31 @@ public class CalendarDB {
 	private static String USERNAME = "root";
 	private static String PASSWORD = "5646";
 	private static String DATABASE = "swdespa_db";
-	
-	public Connection getConnection() {
-		try {
-			Class.forName(DRIVER_NAME);
-			Connection connection = DriverManager.getConnection(
-					URL +
-					DATABASE + "?autoReconnect=true&useSSL=false",
-					USERNAME,
-					PASSWORD);
-			System.out.println("[MYSQL] Connection successful");
-			return connection;
-		} catch (SQLException e) {
-			System.out.println("[MYSQL] Connection failed!");
-			e.printStackTrace();
-			return null;
-		} catch (ClassNotFoundException e) {
-			System.out.println("[MYSQL] Connection failed!");
-			e.printStackTrace();
-			return null;
+
+	private static Connection connection = null;
+
+	private CalendarDB() {
+	}
+
+	public static synchronized Connection getConnection() {
+		if (connection == null) {
+			try {
+				Class.forName(DRIVER_NAME);
+				Connection connection = DriverManager.getConnection(URL + DATABASE + "?autoReconnect=true&useSSL=false",
+						USERNAME, PASSWORD);
+				System.out.println("[MYSQL] Connection successful");
+				return connection;
+			} catch (SQLException e) {
+				System.out.println("[MYSQL] Connection failed!");
+				e.printStackTrace();
+				return null;
+			} catch (ClassNotFoundException e) {
+				System.out.println("[MYSQL] Connection failed!");
+				e.printStackTrace();
+				return null;
+			}
 		}
+		
+		return connection;
 	}
 }
