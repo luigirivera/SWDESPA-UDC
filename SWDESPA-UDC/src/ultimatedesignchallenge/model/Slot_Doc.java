c5a5412e2584e4dd6231b9aaa626149bb9a83842
PaoDLR <36217674+PaoDLR@users.noreply.github.com>
2018-04-13 13:42:33 +0800

    model slot_doc

--- SWDESPA-UDC/src/ultimatedesignchallenge/model/Slot_Doc.java
+++ SWDESPA-UDC/src/ultimatedesignchallenge/model/Slot_Doc.java
@@ -4,4 +4,28 @@ public class Slot_Doc {
 	public static final String TABLE = "SLOT_DOC";
 	public static final String COL_SLOTID = "SLOTid";
 	public static final String COL_DOCTORID = "DOCTORid";
+	
+	private String DocSlotID;
+	private String DocID;
+	
+	public void setDocSlotID(String SlotID) {
+		
+		DocSlotID = SlotID;
+	}
+	
+	public String getDocSlotID() {
+		
+		return DocSlotID;
+	}
+	
+	public void setDocID(String DocID) {
+		
+		this.DocID = DocID;
+	}
+	
+	public String getDocID() {
+		
+		return DocID;
+	}
+	
 }
