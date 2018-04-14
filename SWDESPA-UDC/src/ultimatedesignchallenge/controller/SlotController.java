6ee656ec29956f16e352adb0489cf98a53d1d8df
Luis Lopez <luis_lopez@dlsu.edu.ph>
2018-04-14 16:02:49 +0800

    slots

--- SWDESPA-UDC/src/ultimatedesignchallenge/controller/SlotController.java
+++ SWDESPA-UDC/src/ultimatedesignchallenge/controller/SlotController.java
@@ -1,5 +1,7 @@
 package ultimatedesignchallenge.controller;
 
+import ultimatedesignchallenge.services.SlotService;
+
 public class SlotController {
 	private SlotService service;
 	private /*add the object of the model*/ model;
