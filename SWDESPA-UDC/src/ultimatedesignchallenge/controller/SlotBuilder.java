bcfd30b2daee3f71ba0015106f7233a2557a6218
Rolopens <rolopens@gmail.com>
2018-04-16 00:04:23 +0800

    FIXED BUILDER

--- SWDESPA-UDC/src/ultimatedesignchallenge/controller/SlotBuilder.java
+++ SWDESPA-UDC/src/ultimatedesignchallenge/controller/SlotBuilder.java
@@ -1,42 +1,43 @@
 package ultimatedesignchallenge.controller;
 
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import ultimatedesignchallenge.model.Slot;
 import ultimatedesignchallenge.model.Client;
 import ultimatedesignchallenge.model.Doctor;
 import ultimatedesignchallenge.services.DoctorService;
 
 public class SlotBuilder {
 
-	public List<SlotC> buildAvailable(LocalDateTime startTime, LocalDateTime endTime, Doctor doctor) { //change if needed (slotC because its the slot in controller)
-		List<SlotC> slots = new ArrayList<SlotC>();
+    public List<Slot> buildSlots(LocalDateTime startTime, LocalDateTime endTime) { //change if needed (slotC because its the slot in controller)
+		List<Slot> slots = new ArrayList<Slot>();
 		while (startTime.isBefore(endTime)) {
-			slots.add(new AvailableInterval(startTime, startTime.plusMinutes(30), doctor));
+			Slot tempSlot = new Slot();
+			tempSlot.setStart(startTime);
+			tempSlot.setEnd(endTime);
+			tempSlot.setIsRecurring(false);
+			slots.add(tempSlot);
 			startTime = startTime.plusMinutes(30);
 		}
 		return slots;
 	}
 	
-	public SlotC buildAppointment(LocalDateTime startTime, LocalDateTime endTime, Doctor doctor, Client client) {
-		SlotC slot;
-		slot = new AppointmentInterval(startTime, endTime, doctor, client);
-		return slot;
-	}
-	
-	public static void main(String[] args) {
-		DoctorService dsv = new DoctorService();
-		SlotBuilder sb = new SlotBuilder();
-		LocalDate datetoday = LocalDate.now();
-		LocalTime start = LocalTime.of(13, 0);
-		LocalTime end = LocalTime.of(15, 0);
-		List<SlotC> buildAvailable = sb.buildAvailable(LocalDateTime.of(datetoday, start), LocalDateTime.of(datetoday, end), dsv.getDoctor(1));
-		for (SlotC slotc : buildAvailable) {
-			System.out.println(slotc);
+	public List<Slot> buildRecurringSlots(LocalDateTime startTime, LocalDateTime endTime) { //change if needed (slotC because its the slot in controller)
+		List<Slot> slots = new ArrayList<Slot>();
+		while (startTime.isBefore(endTime)) {
+			Slot tempSlot = new Slot();
+			tempSlot.setStart(startTime);
+			tempSlot.setEnd(endTime);
+			tempSlot.setIsRecurring(true);
+			slots.add(tempSlot);
+			startTime = startTime.plusMinutes(30);
 		}
+		return slots;
 	}
-	
 }
